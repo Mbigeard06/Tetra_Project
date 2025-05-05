b86aec7f4533c376f77c833e4df350911913b9dc
Matéo <115616225+Mbigeard06@users.noreply.github.com>
2025-05-05 15:43:40 +0200

    Update train.py

--- src/tetra/model/train.py
+++ src/tetra/model/train.py
@@ -1,16 +1,29 @@
 from ultralytics import YOLO
 
 # Load a base model (or your previously trained checkpoint)
-model = YOLO('yolov8n.pt')  # or 'runs/detect/train/weights/best.pt'
+model = YOLO('yolo11m.pt')  # or 'runs/detect/train/weights/best.pt'
 
 # Train (finetune) with evolved hyperparameters
 model.train(
-    data="lyrurus.yaml",
+    data="../../../dataset_sliced/2025-05-02_5-Fold_Cross-val/split_1/split_1_dataset.yaml",
     epochs=100,
+    time = 15,
     imgsz=640,
+    patience = 10,
     batch=16,
-    hyp="runs/tune/lyrurus_tune/hyp_evolved.yaml",
+    save = True,
+    device="cpu",
+    project="./runs/train",
     name="lyrurus_finetuned",
-    project="runs/train",
-    device= '0'
+    exist_ok=True,
+    pretrained=True,
+    optimizer="AdamW",
+    single_cls=True,
+    multi_scale = False,
+    cos_lr = True,
+    fraction = 0.1, #TO change
+    #freeze to change for fine tuning,
+    lr0 = 1e-3,
+    lrf = 0.01,
+    plots = True
 )
