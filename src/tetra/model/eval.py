da9fd6783698b5e09f74282058be837d73f415b8
Mbigeard06 <mateo.bd@icloud.com>
2025-05-13 14:23:22 +0200

    Update gitignore

--- src/tetra/model/eval.py
+++ src/tetra/model/eval.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load the model
-model = YOLO("../../../../runs/train/lyrurus_yolov11m_new_dataset/weights/best.pt")
+model = YOLO("../../../../runs/train/lyrurus_yolov8m_new_dataset/weights/best.pt")
 
 # Run the evaluation with proper params
 results = model.val(
@@ -17,9 +17,3 @@ results = model.val(
 )
 
 
-# Print specific metrics
-print(f"F1-score global : {results.box.f1:.4f}")
-print(f"mAP@0.5        : {results.box.map50:.4f}")
-print(f"mAP@0.5:0.95   : {results.box.map:.4f}")
-print(f"Précision      : {results.box.precision:.4f}")
-print(f"Rappel         : {results.box.recall:.4f}")
